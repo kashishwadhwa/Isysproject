383c7b1d7ddc828359411c8b756ed8679b7ddabf
shashaank mathur <mathur.shashaank@gmail.com>
2016-04-21 18:25:13 -0500

    vhkbjb

--- WebApplication1/dashboard.aspx.designer.cs
+++ WebApplication1/dashboard.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace WebApplication1 {
         protected global::System.Web.UI.WebControls.DropDownList ddlCountries;
         
         /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
         /// rblChartType control.
         /// </summary>
         /// <remarks>
